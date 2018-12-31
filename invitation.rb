7205ea8e687a3806dfb42a06d1eaf2add7e1273a
gvays <gabbyvays@gmail.com>
2018-12-31 17:33:27 +0000

    Automatically backed up by Learn

--- invitation.rb
+++ invitation.rb
@@ -17,4 +17,3 @@ time = gets.chomp
 host_name = gets.chomp
 
 puts "Hi, you've been invited to a party! What is your name #{guest_name}. What is the party called? The party is #{party_name}"
-#{party_name} = Best Halloween Party Ever
